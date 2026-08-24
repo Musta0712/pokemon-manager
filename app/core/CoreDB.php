d560e93e0f596ab4268828fdc35709b323431ef7
Musta0712 <adrimustatea@gmail.com>
2026-08-24 15:06:28 +0200

    .

--- app/core/CoreDB.php
+++ app/core/CoreDB.php
@@ -1,29 +1,27 @@
 <?php
 class CoreDB{
 
-    static function getConnection(): mysqli{
-        $configPath = __DIR__ . '/config.php';
+    static function getConnection(): mysqli {
+    $configPath = __DIR__ . '/config.php';
 
-        if (!file_exists($configPath)) {
-            throw new Exception(
-                'Falta app/core/config.php. Copia config.example.php como config.php y rellena tus datos.'
-            );
-        }
-
-        $config = require $configPath;
-
-        $conn = new mysqli(
-            $config['db_host'],
-            $config['db_user'],
-            $config['db_pass'],
-            $config['db_name'],
-            3306
-        );
-
-        if ($conn->connect_error) {
-            throw new Exception('Error de conexión a la base de datos: ' . $conn->connect_error);
-        }
-
-        return $conn;
+    if (!file_exists($configPath)) {
+        throw new Exception('No existe el archivo en: ' . $configPath);
     }
+
+    $config = require $configPath;
+
+    // Esto nos mostrará a qué host se está intentando conectar realmente
+    $conn = @new mysqli(
+        $config['db_host'],
+        $config['db_user'],
+        $config['db_pass'],
+        $config['db_name']
+    );
+
+    if ($conn->connect_error) {
+        throw new Exception("Intento de conexión a {$config['db_host']} falló: " . $conn->connect_error);
+    }
+
+    return $conn;
+}
 }
